999bfc46d832b5d70de153be1d546f22ef2f96fa
OpenDental <itdepartment@opendental.com>
2022-09-12 13:32:40 -0700

    version 21.3.61.0

--- CodeBase/Properties/AssemblyInfo.cs
+++ CodeBase/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("21.3.60.0")]
+[assembly: AssemblyVersion("21.3.61.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
