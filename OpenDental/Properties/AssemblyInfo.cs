1b5339c4f7209a1306e3410ddbfcf22358f4eb2a
Administrator <admin@example.com>
2024-08-30 12:49:38 -0700

    version 24.1.67.0

--- OpenDental/Properties/AssemblyInfo.cs
+++ OpenDental/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 //so we might consider removing it when a significant portion of OD is in WPF.
 [assembly: System.Windows.Media.DisableDpiAwareness]
 
-[assembly: AssemblyVersion("24.1.66.0")]
+[assembly: AssemblyVersion("24.1.67.0")]
 
 //[assembly: AssemblyFileVersion("1.0.0.0")]
